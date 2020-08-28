bfd5ab2cf7e1f4eaae31ce5e75de9e45985c15ca
prikhodina@fzi.de <!E1Dd0702?>
2020-08-28 12:25:29 +0200

    add def 'name' and class 'PDFReportImages'

--- PDFReport.py
+++ PDFReport.py
@@ -2,7 +2,7 @@ import pandas as pd
 from fpdf import FPDF
 import numpy as np
 
-class PDFReportMaker(FPDF):
+class PDFReport(FPDF):
 
     def header(self):
         self.set_font('arial', 'I', 12)
@@ -15,13 +15,13 @@ class PDFReportMaker(FPDF):
         self.set_y(-20)
         self.cell(0, 10, 'Placeholder for the footer/Page ' + str(self.page_no()), 1, 0, 'C')
 
-    #def title(self, title):
-        #self.set_xy(0, 0)
-        #self.set_font('arial', 'B', 20)
-        #self.cell(10)  # move to 10 cm to the right
-        #self.cell(0, 50, " ", 1, 2, 'C')
-        #self.cell(0, 20, str(title), 1, 2, 'C')
-        #self.cell(0, 15, " ", 1, 2, 'C')
+    def name(self, name):
+        self.set_xy(0, 0)
+        self.set_font('arial', 'B', 20)
+        self.cell(10)  # move to 10 cm to the right
+        self.cell(0, 50, " ", 1, 2, 'C')
+        self.cell(0, 20, '%s' % str(name), 1, 2, 'C')
+        self.cell(0, 15, " ", 1, 2, 'C')
 
     def table(self, df):
         self.set_xy(10, 85)
@@ -43,20 +43,20 @@ class PDFReportMaker(FPDF):
         self.ln(10)
 
     def insert_image(self, image):
-        self.image('image.png', x=130, y= 90, w=40, type='', link='')
+        self.image(image, x=None, y=None, w=40, type='', link='') #x=130, y=90
 
     def write_left_from_textfile(self, txtfile):
-        pdf.set_xy(10, 155)
-        pdf.set_font('arial', '', 12)
+        self.set_xy(10, 155)
+        self.set_font('arial', '', 12)
         f = open(txtfile, "r")
         for x in f:
-            pdf.multi_cell(w=90, h=10, txt=x, border=1,
+            self.multi_cell(w=90, h=10, txt=x, border=1,
                            align='L', fill=False)
 
     def write_right(self, text):
-        pdf.set_xy(110, 155)
-        pdf.set_font('arial', '', 12)
-        pdf.multi_cell(w=90, h=10, txt=text, border=1,
+        self.set_xy(110, 155)
+        self.set_font('arial', '', 12)
+        self.multi_cell(w=90, h=10, txt=text, border=1,
                        align='L', fill=False)
 
     def write_from_excel(self):
@@ -66,61 +66,54 @@ class PDFReportMaker(FPDF):
         writer.save()
 
         df_2 = pd.read_excel('testtable.xlsx')
-        pdf.set_font('arial', 'B', 12)
+        self.set_font('arial', 'B', 12)
         #pdf.cell(10)
-        pdf.cell(70, 10, 'Writing a table from excel', 0, 2, 'C')
+        self.cell(70, 10, 'Writing a table from excel', 0, 2, 'C')
         # pdf.cell(-40)
-        pdf.cell(30, 10, 'Index Column', 1, 0, 'C')
-        pdf.cell(30, 10, 'Col A', 1, 0, 'C')
-        pdf.cell(30, 10, 'Col B', 1, 2, 'C')
-        pdf.cell(-60)
-        pdf.set_font('arial', '', 12)
+        self.cell(30, 10, 'Index Column', 1, 0, 'C')
+        self.cell(30, 10, 'Col A', 1, 0, 'C')
+        self.cell(30, 10, 'Col B', 1, 2, 'C')
+        self.cell(-60)
+        self.set_font('arial', '', 12)
 
         for i in range(0, len(df_2) - 1):
-            pdf.cell(30, 10, '%s' % str(i), 1, 0, 'C')
-            pdf.cell(30, 10, '%s' % str(df_2.A.iloc[i]), 1, 0, 'C')
-            pdf.cell(30, 10, '%s' % str(df_2.B.iloc[i]), 1, 2, 'C')
-            pdf.cell(-60)
+            self.cell(30, 10, '%s' % str(i), 1, 0, 'C')
+            self.cell(30, 10, '%s' % str(df_2.A.iloc[i]), 1, 0, 'C')
+            self.cell(30, 10, '%s' % str(df_2.B.iloc[i]), 1, 2, 'C')
+            self.cell(-60)
 
 df = pd.DataFrame()
 df['Number'] = ["1", "2", "3", "4"]
 df['A'] = [3, 4, 5, 3]
 df['B'] = [3, 3, 4, 4]
 
-title = "Name of the report"
-image = 'image.png'
+name = "Name of the report"
+#image = 'image.png'
 txtfile = 'text.txt'
 text = "Placeholder for the text. Overwrite the line with your text." \
        "Placeholder for the text. Overwrite the line with your text." \
        "Placeholder for the text. Overwrite the line with your text." \
        "Placeholder for the text. Overwrite the line with your text." \
        "Placeholder for the text. Overwrite the line with your text." \
-        "Placeholder for the text. Overwrite the line with your text." \
+       "Placeholder for the text. Overwrite the line with your text." \
        "Placeholder for the text. Overwrite the line with your text." \
        "Placeholder for the text. Overwrite the line with your text." \
        "Placeholder for the text. Overwrite the line with your text." \
        "Placeholder for the text. Overwrite the line with your text." \
 
-
-pdf = PDFReportMaker()
+pdf = PDFReport()
 pdf.add_page()
-pdf.set_margins(left=10, top=10, right=10)
-#pdf.line(105, 0, 105, 297) #line in the middle of the page A4 (210 mm * 297 mm)
+# pdf.set_margins(left=10, top=10, right=10)
+# #pdf.line(105, 0, 105, 297) #line in the middle of the page A4 (210 mm * 297 mm)
 pdf.set_xy(0, 0)
 
-pdf.set_font('arial', 'B', 20)
-pdf.cell(10) #move to 10 cm to the right
-pdf.cell(0, 50, " ", 0, 2, 'C')
-pdf.cell(0, 20, title, 1, 2, 'C')
-pdf.cell(0, 15, " ", 0, 2, 'C')
+# pdf.title(title)
+# pdf.table(df)
+# #pdf.insert_image(image)
+# pdf.write_left_from_textfile(txtfile)
+# pdf.write_right(text)
+#
+# pdf.ln(20)
+# pdf.write_from_excel()
 
-#pdf.title(title)
-pdf.table(df)
-pdf.insert_image(image)
-pdf.write_left_from_textfile(txtfile)
-pdf.write_right(text)
-
-pdf.ln(20)
-pdf.write_from_excel()
-
-pdf.output('Report.pdf', dest='F') #local file
+# pdf.output('Report.pdf', dest='F') #local file
